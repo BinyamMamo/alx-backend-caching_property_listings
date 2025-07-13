a1798476c6a6a16dc7f00d138416f176c48c3ff0
BinyamMamo <binyammamo01@gmail.com>
2025-07-13 22:53:18 +0400

    Task 2: Low-level caching with get_all_properties utility

--- properties/views.py
+++ properties/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 
 
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     """
     View to return all properties with 15-minute caching
+    Uses low-level cache API for queryset caching
     """
-    properties = Property.objects.all()
+    properties = get_all_properties()
     data = []
     
     for prop in properties:
